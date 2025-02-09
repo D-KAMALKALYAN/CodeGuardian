230ae2d3f13bfa53c62845568aab6b0205e7c036
sandeep5599 <sandeepvadlamudi999@gmail.com>
2025-02-09 15:20:21 +0530

    updated server.js with frontend env link, package.json with correct url

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Frontend URL
+  credentials: true
+}));
+
 
 app.use('/api/auth', authRoutes);
 
