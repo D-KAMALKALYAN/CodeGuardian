829634d6e7c371e9733ceb0509ced58d38196f2b
Kamal <kamalkalyan1260@gmail.com>
2025-05-02 22:30:20 +0530

    backend prod url update

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,6 +3,6 @@
 const prod_URL = "https://code-guardian-backend.onrender.com";
 const dev_URL = "http://localhost:5000";    
 
-const API_BASE_URL = dev_URL; // Change this to prod_URL when deploying to production
+const API_BASE_URL = prod_URL; // Change this to prod_URL when deploying to production
 
 export default API_BASE_URL;
